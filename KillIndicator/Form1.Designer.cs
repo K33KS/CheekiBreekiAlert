4bc687a02f0e87d09d4ab74b66f610032637df4d
Keenan DeAngelis <kdeangelis@findawayworld.com>
2018-09-05 21:20:57 -0400

    FIrst Commit

---
+++ KillIndicator/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace KillIndicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxHighlightsPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSoundPath = new System.Windows.Forms.TextBox();
+            this.btnBrowseSound = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBoxCheeki = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCheeki)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxHighlightsPath
+            // 
+            this.textBoxHighlightsPath.Location = new System.Drawing.Point(12, 30);
+            this.textBoxHighlightsPath.Name = "textBoxHighlightsPath";
+            this.textBoxHighlightsPath.Size = new System.Drawing.Size(400, 20);
+            this.textBoxHighlightsPath.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSoundPath
+            // 
+            this.textBoxSoundPath.Location = new System.Drawing.Point(12, 73);
+            this.textBoxSoundPath.Name = "textBoxSoundPath";
+            this.textBoxSoundPath.Size = new System.Drawing.Size(400, 20);
+            this.textBoxSoundPath.TabIndex = 0;
+            // 
+            // btnBrowseSound
+            // 
+            this.btnBrowseSound.Location = new System.Drawing.Point(420, 71);
+            this.btnBrowseSound.Name = "btnBrowseSound";
+            this.btnBrowseSound.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseSound.TabIndex = 1;
+            this.btnBrowseSound.Text = "Browse...";
+            this.btnBrowseSound.UseVisualStyleBackColor = true;
+            this.btnBrowseSound.Click += new System.EventHandler(this.btnBrowseSound_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path to Nvidia Highlights (Sorry AMDfags)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Path to Sound File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRun.Location = new System.Drawing.Point(132, 117);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(224, 43);
+            this.buttonRun.TabIndex = 3;
+            this.buttonRun.Text = "RUN";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Tarkov Kill Indicator - STOPPED";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pictureBoxCheeki
+            // 
+            this.pictureBoxCheeki.Image = global::KillIndicator.Properties.Resources.cheeki;
+            this.pictureBoxCheeki.Location = new System.Drawing.Point(395, 99);
+            this.pictureBoxCheeki.Name = "pictureBoxCheeki";
+            this.pictureBoxCheeki.Size = new System.Drawing.Size(100, 90);
+            this.pictureBoxCheeki.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCheeki.TabIndex = 4;
+            this.pictureBoxCheeki.TabStop = false;
+            this.pictureBoxCheeki.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 188);
+            this.Controls.Add(this.pictureBoxCheeki);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowseSound);
+            this.Controls.Add(this.textBoxSoundPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxHighlightsPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tarkov Kill Indicator";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCheeki)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxHighlightsPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSoundPath;
+        private System.Windows.Forms.Button btnBrowseSound;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonRun;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBoxCheeki;
+    }
+}
+
---
+++ KillIndicator/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KillIndicator
+{
+    public partial class Form1 : Form
+    {
+        public string pathToHighlights = Properties.Settings.Default.pathToHighlights;
+        public string pathToSound = Properties.Settings.Default.pathToSound;
+        public bool running = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxHighlightsPath.Text = pathToHighlights;
+            textBoxSoundPath.Text = pathToSound;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBoxHighlightsPath.Text = fbd.SelectedPath.ToString();
+                    pathToHighlights = fbd.SelectedPath.ToString();
+                    Properties.Settings.Default.pathToHighlights = fbd.SelectedPath.ToString();
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void btnBrowseSound_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                DialogResult result = ofd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ofd.FileName))
+                {
+                    textBoxSoundPath.Text = ofd.FileName;
+                    pathToSound = ofd.FileName;
+                    Properties.Settings.Default.pathToSound = ofd.FileName;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            ToggleRun();
+        }
+
+        private void ToggleRun()
+        {
+            if (running)
+            {
+                running = false;
+                buttonRun.Text = "RUN";
+                buttonRun.ForeColor = System.Drawing.Color.Black;
+                btnBrowseSound.Enabled = true;
+                button1.Enabled = true;
+                textBoxHighlightsPath.Enabled = true;
+                textBoxSoundPath.Enabled = true;
+                pictureBoxCheeki.Visible = false;
+                notifyIcon1.Text = "Tarkov Kill Indicator - STOPPED";
+                if(backgroundWorker1.IsBusy)
+                backgroundWorker1.Dispose();
+            }
+            else
+            {
+                running = true;
+                buttonRun.Text = "STOP";
+                buttonRun.ForeColor = System.Drawing.Color.Red;
+                backgroundWorker1.RunWorkerAsync();
+                button1.Enabled = false;
+                btnBrowseSound.Enabled = false;
+                textBoxHighlightsPath.Enabled = false;
+                textBoxSoundPath.Enabled = false;
+                pictureBoxCheeki.Visible = true;
+                notifyIcon1.Text = "Tarkov Kill Indicator - RUNNING";
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            if (Directory.Exists(pathToHighlights))
+            {
+                DetectKills();
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid path to Nvidia Highlights temp directory.");
+                ToggleRun();
+            }
+        }
+
+        private void DetectKills()
+        {
+            int newCount = 0;
+            int oldCount = 0;
+            while (running)
+            {
+                try
+                {
+                    newCount = Directory.GetFiles(pathToHighlights).Length;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Please select a valid path to Nvidia Highlights temp directory.\n\n" + e.Message);
+                    break;
+                }
+                if (oldCount < newCount)
+                {
+                    oldCount = newCount;
+                    PlaySound();
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(500);
+                }
+            }
+        }
+
+        private void PlaySound()
+        {
+            try
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(pathToSound);
+                player.Play();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Could not play selected sound file.\n\n" + e.Message);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
